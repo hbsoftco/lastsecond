fca5047fc623e302414dd1372922e34ce3f767a4
Hossein <hossein.bajan@gmail.com>
2025-03-08 16:12:48 +0330

    chore(dependencies): add tailwind css as a dependency

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-8xl text-center font-black text-red-600">
+    hi
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  devtools: { enabled: true },
+
+  modules: [],
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
