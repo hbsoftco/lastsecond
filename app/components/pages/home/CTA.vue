07fb22a32bb303ffcb4d3e771432bfe44775d6a4
Hossein <hossein.bajan@gmail.com>
2025-03-11 10:58:51 +0330

    feat: add call to action button compoent

---
+++ app/components/pages/home/CTA.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="bg-blue-accent-400 hover:bg-blue-accent-500 transition-all duration-200 text-white p-3 rounded-full px-6 font-medium cursor-pointer shadow-lg items-center">
+    <span class="flex justify-between items-center px-4">
+      <span class="ml-4">شروع سفر به ناکجا</span>
+      <Icon
+        name="material-symbols-light:travel"
+        size="28"
+      />
+    </span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
