d1a7600f3db9d851d401fdf3597d77cc208515ef
Hossein <hossein.bajan@gmail.com>
2025-03-11 12:38:15 +0330

    feat(tours): add popular tours box component

---
+++ app/components/common/PopularTour.vue
@@ -0,0 +1,58 @@
+<template>
+  <NuxtLink
+    :to="link"
+    class="relative rounded-xl overflow-hidden w-60 h-60"
+  >
+    <div class="w-60 h-60">
+      <NuxtImg
+        provider="ipx"
+        :src="src"
+        :quality="quality"
+        :height="height"
+        :width="width"
+        format="webp"
+        loading="lazy"
+        :modifiers="{ w: width, h: height }"
+        class="w-full h-full object-cover"
+      />
+    </div>
+    <div
+      class="absolute bottom-0 p-3 flex items-end w-full h-40 bg-linear-to-t from-gray-blue-1000 from-10% to-transparent to-90%"
+    >
+      <div>
+        <h5 class="font-black text-2xl text-gray-blue-100 mb-2">
+          {{ departure }}
+        </h5>
+        <p class="text-gray-blue-50 text-xs space-x-2">
+          <span>{{ $t('common.priceStartsFrom') }}:</span>
+          <strong class="text-white text-base font-black">{{ price }}</strong>
+          <span>{{ $t('currencies.irt') }}</span>
+        </p>
+      </div>
+    </div>
+    <div class="absolute top-0.5 right-1">
+      <span class="bg-gray-blue-50 text-xs font-medium rounded text-gray-blue-1000 px-1.5 py-0.5 shadow rounded-tr-xl">
+        {{ tourCount }} {{ $t('common.tour') }}
+      </span>
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+interface PropsDefinition {
+  src: string;
+  link: string;
+  tourCount: string;
+  departure: string;
+  price: string;
+  height?: number;
+  width?: number;
+  quality?: number;
+}
+
+withDefaults(defineProps<PropsDefinition>(), {
+  height: 372,
+  width: 372,
+  quality: 100,
+});
+</script>
