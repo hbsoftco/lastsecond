7186ede924c04bc9b2c523c6650dfecffdf87af8
Hossein <hossein.bajan@gmail.com>
2025-03-11 12:41:07 +0330

    feat: add new component for displying images in top search

---
+++ app/components/pages/home/FeaturedDestination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative p-2">
+    <div class="bg-linear-to-b from-gray-blue-500 to-gray-blue-300 rounded-full p-3">
+      <NuxtImg
+        :src="src"
+        :quality="quality"
+        :height="height"
+        :width="width"
+        format="webp"
+        class="relative rounded-full border-8 border-gray-blue-600"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface PropsDefinition {
+  src: string;
+  height?: number;
+  width?: number;
+  quality?: number;
+}
+
+withDefaults(defineProps<PropsDefinition>(), {
+  height: 280,
+  width: 210,
+  quality: 80,
+});
+</script>
