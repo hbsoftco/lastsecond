4084fa2f488e43c4b62917a638eda39112475d4d
Hossein <hossein.bajan@gmail.com>
2025-03-15 17:12:10 +0330

    feat: show special tours and popular tours into carousel

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,109 +37,71 @@
 
     <div class="l-container pb-24">
       <h2 class="text-2xl font-black mb-8">
-        تور از مبدا شهرهای پرطرفدار
+        {{ $t('pages.home.popularDepartureTours') }}
       </h2>
-      <div class="flex space-x-4">
-        <PopularTour
-          src="https://last-cdn.com/2025/03/10/iGjhEu8R4QU2fmEQpzs8GsmPpeRQnzmeNJKuGXpG.jpg"
-          :width="500"
-          :height="372"
-          price="۳۶,۶۰۰,۰۰۰"
-          link="/"
-          tour-count="۱۰۰"
-          departure="از تهران"
-        />
-        <PopularTour
-          src="https://last-cdn.com/2025/02/12/bdbm76Y1nvbjWLjSMPNM26Iiz2UyTb0YtDN7Ux2Y.jpg"
-          :width="500"
-          :height="372"
-          price="۳۶,۶۰۰,۰۰۰"
-          link="/"
-          tour-count="۴۳۳"
-          departure="از مشهد"
-        />
-        <PopularTour
-          src="https://last-cdn.com/2025/02/16/JgBNTlhTrv8jUpKwQeGWJBWpqsr36MnqDuTO9wVd.jpg"
-          :width="500"
-          :height="372"
-          price="۳۶,۶۰۰,۰۰۰"
-          link="/"
-          tour-count="۶۷"
-          departure="از شیراز"
-        />
-        <PopularTour
-          src="https://last-cdn.com/2024/09/16/b6P6gPWvDfo1zF93Ff6s8v9BxEFDnmMEi4n2CerM.jpg"
-          :width="500"
-          :height="372"
-          price="۳۶,۶۰۰,۰۰۰"
-          link="/"
-          tour-count="۸۸"
-          departure="از اصفهان"
-        />
-        <PopularTour
-          src="https://last-cdn.com/2025/02/08/05hyDiwcQubgLx6kEhkZRv0AmVBHMatW8RLJmRsd.jpg"
-          :width="500"
-          :height="372"
-          price="۳۶,۶۰۰,۰۰۰"
-          link="/"
-          tour-count="۲۲"
-          departure="از کاشان"
-        />
+      <div dir="ltr">
+        <Carousel
+          v-bind="carouselConfig"
+        >
+          <Slide
+            v-for="tour in popularTours"
+            :key="tour"
+          >
+            <div>
+              <PopularTour
+                :src="tour.src"
+                :width="tour.width"
+                :height="tour.height"
+                :price="tour.price"
+                :link="tour.link"
+                :tour-count="tour.tourCount"
+                :departure="tour.departure"
+              />
+            </div>
+          </Slide>
+
+          <template #addons>
+            <Navigation />
+          </template>
+        </Carousel>
       </div>
     </div>
     <!-- Popular departure city  -->
 
     <div class="l-container pb-96">
       <h2 class="text-2xl font-black mb-8">
-        تورهای ویژه
+        {{ $t('pages.home.specialTours') }}
       </h2>
-      <div class="flex space-x-4">
-        <GridTour
-          src="https://last-cdn.com/2017/11/08/kxUln202ezwmZNxtNA6RWahHJqZY39hCCF1IsYip--300x.jpeg"
-          :width="288"
-          :height="176"
-          price="۳۲۱,۶۰۰,۰۰۰"
-          dollar-price="۴۵۰"
-          night-count="۵"
-          link="/"
-          airline="پگاسوس"
-          title="تور کوالالامپور"
-          agency="گلفام سفر"
-        />
-        <GridTour
-          src="https://last-cdn.com/2025/02/20/kw3NsRYZOHIeC84dpdjr94G2IuvbFnE6SjzVgzP2--300x.jpg"
-          :width="288"
-          :height="176"
-          price="۹۳,۶۰۰,۰۰۰"
-          night-count="۶"
-          link="/"
-          airline="معراج"
-          title="تور ویتنام نورور ۱۴۰۴ (مستقیم)"
-          agency="آویسا الهه باستان"
-        />
-        <GridTour
-          src="https://last-cdn.com/2017/11/08/GG5iaYFEyzxNcS6z68UPX1ikl5sa9FZYs8CjNRPN--300x.jpeg"
-          :width="288"
-          :height="176"
-          price="۳۶,۶۰۰,۰۰۰"
-          night-count="۴"
-          link="/"
-          airline="ایران ایرتور"
-          title="تور تونس"
-          agency="سورتمه گشت آریا"
-        />
-        <GridTour
-          src="https://last-cdn.com/2024/03/10/SNixmVfKwmqDdLBKKpJi3Y01IqFaL7XixFqBtkIs--300x.jpg"
-          :width="288"
-          :height="176"
-          price="۱۰۲,۶۰۰,۰۰۰"
-          dollar-price="۴۵۰"
-          night-count="۳"
-          link="/"
-          airline="ایر عربیا"
-          title="تور کوالالامپور + سنگاپور نوروز ۱۴۰۴"
-          agency="چکاوک"
-        />
+      <div>
+        <Carousel
+          v-bind="tourCarouselConfig"
+          dir="ltr"
+        >
+          <Slide
+            v-for="tour in tours"
+            :key="tour"
+          >
+            <div>
+              <GridTour
+                :src="tour.src"
+                :width="tour.width"
+                :height="tour.height"
+                :price="tour.price"
+                :dollar-price="tour.dollarPrice"
+                :night-count="tour.nightCount"
+                :link="tour.link"
+                :airline="tour.airline"
+                :title="tour.title"
+                :agency="tour.agency"
+                :min-width="true"
+              />
+            </div>
+          </Slide>
+
+          <template #addons>
+            <Navigation />
+          </template>
+        </Carousel>
       </div>
     </div>
     <!-- Special tours  -->
@@ -147,6 +109,8 @@
 </template>
 
 <script lang="ts" setup>
+import 'vue3-carousel/carousel.css';
+
 import FeaturedDestination from '~/components/pages/home/FeaturedDestination.vue';
 import PopularTour from '~/components/common/PopularTour.vue';
 import TopSearch from '~/components/pages/home/TopSearch.vue';
@@ -158,4 +122,152 @@ const featuredDestinations = [
   { src: 'https://cdn.lastsecond.ir/last6-assets/img/02.b25be74.jpg' },
   { src: 'https://cdn.lastsecond.ir/last6-assets/img/01.681d4fa.jpg' },
 ];
+
+const tours = [
+  {
+    src: 'https://last-cdn.com/2017/11/08/kxUln202ezwmZNxtNA6RWahHJqZY39hCCF1IsYip--300x.jpeg',
+    width: 288,
+    height: 176,
+    price: '۳۲۱,۶۰۰,۰۰۰',
+    dollarPrice: '۴۵۰',
+    nightCount: '۵',
+    link: '/',
+    airline: 'پگاسوس',
+    title: 'تور کوالالامپور',
+    agency: 'گلفام سفر',
+  },
+  {
+    src: 'https://last-cdn.com/2025/02/20/kw3NsRYZOHIeC84dpdjr94G2IuvbFnE6SjzVgzP2--300x.jpg',
+    width: 288,
+    height: 176,
+    price: '۹۳,۶۰۰,۰۰۰',
+    nightCount: '۶',
+    link: '/',
+    airline: 'معراج',
+    title: 'تور ویتنام نورور ۱۴۰۴ (مستقیم)',
+    agency: 'آویسا الهه باستان',
+  },
+  {
+    src: 'https://last-cdn.com/2017/11/08/GG5iaYFEyzxNcS6z68UPX1ikl5sa9FZYs8CjNRPN--300x.jpeg',
+    width: 288,
+    height: 176,
+    price: '۳۶,۶۰۰,۰۰۰',
+    nightCount: '۴',
+    link: '/',
+    airline: 'ایران ایرتور',
+    title: 'تور تونس',
+    agency: 'سورتمه گشت آریا',
+  },
+  {
+    src: 'https://last-cdn.com/2024/03/10/SNixmVfKwmqDdLBKKpJi3Y01IqFaL7XixFqBtkIs--300x.jpg',
+    width: 288,
+    height: 176,
+    price: '۱۰۲,۶۰۰,۰۰۰',
+    dollarPrice: '۴۵۰',
+    nightCount: '۳',
+    link: '/',
+    airline: 'ایر عربیا',
+    title: 'تور کوالالامپور + سنگاپور نوروز ۱۴۰۴',
+    agency: 'چکاوک',
+  },
+  {
+    src: 'https://last-cdn.com/2025/02/20/kw3NsRYZOHIeC84dpdjr94G2IuvbFnE6SjzVgzP2--300x.jpg',
+    width: 288,
+    height: 176,
+    price: '۹۳,۶۰۰,۰۰۰',
+    nightCount: '۶',
+    link: '/',
+    airline: 'معراج',
+    title: 'تور ویتنام نورور ۱۴۰۴ (مستقیم)',
+    agency: 'آویسا الهه باستان',
+  },
+  {
+    src: 'https://last-cdn.com/2017/11/08/GG5iaYFEyzxNcS6z68UPX1ikl5sa9FZYs8CjNRPN--300x.jpeg',
+    width: 288,
+    height: 176,
+    price: '۳۶,۶۰۰,۰۰۰',
+    nightCount: '۴',
+    link: '/',
+    airline: 'ایران ایرتور',
+    title: 'تور تونس',
+    agency: 'سورتمه گشت آریا',
+  },
+  {
+    src: 'https://last-cdn.com/2024/03/10/SNixmVfKwmqDdLBKKpJi3Y01IqFaL7XixFqBtkIs--300x.jpg',
+    width: 288,
+    height: 176,
+    price: '۱۰۲,۶۰۰,۰۰۰',
+    dollarPrice: '۴۵۰',
+    nightCount: '۳',
+    link: '/',
+    airline: 'ایر عربیا',
+    title: 'تور کوالالامپور + سنگاپور نوروز ۱۴۰۴',
+    agency: 'چکاوک',
+  },
+];
+
+const popularTours = [
+  {
+    src: 'https://last-cdn.com/2025/03/10/iGjhEu8R4QU2fmEQpzs8GsmPpeRQnzmeNJKuGXpG.jpg',
+    width: 500,
+    height: 372,
+    price: '۳۶,۶۰۰,۰۰۰',
+    link: '/',
+    tourCount: '۱۰۰',
+    departure: 'از تهران',
+  },
+  {
+    src: 'https://last-cdn.com/2025/02/12/bdbm76Y1nvbjWLjSMPNM26Iiz2UyTb0YtDN7Ux2Y.jpg',
+    width: 500,
+    height: 372,
+    price: '۳۶,۶۰۰,۰۰۰',
+    link: '/',
+    tourCount: '۴۳۳',
+    departure: 'از مشهد',
+  },
+  {
+    src: 'https://last-cdn.com/2025/02/16/JgBNTlhTrv8jUpKwQeGWJBWpqsr36MnqDuTO9wVd.jpg',
+    width: 500,
+    height: 372,
+    price: '۳۶,۶۰۰,۰۰۰',
+    link: '/',
+    tourCount: '۶۷',
+    departure: 'از شیراز',
+  },
+  {
+    src: 'https://last-cdn.com/2024/09/16/b6P6gPWvDfo1zF93Ff6s8v9BxEFDnmMEi4n2CerM.jpg',
+    width: 500,
+    height: 372,
+    price: '۳۶,۶۰۰,۰۰۰',
+    link: '/',
+    tourCount: '۸۸',
+    departure: 'از اصفهان',
+  },
+  {
+    src: 'https://last-cdn.com/2025/02/08/05hyDiwcQubgLx6kEhkZRv0AmVBHMatW8RLJmRsd.jpg',
+    width: 500,
+    height: 372,
+    price: '۳۶,۶۰۰,۰۰۰',
+    link: '/',
+    tourCount: '۲۲',
+    departure: 'از کاشان',
+  },
+];
+
+const carouselConfig = {
+  itemsToShow: 5,
+  wrapAround: true,
+  breakpoints: {
+    300: { itemsToShow: 1 },
+    400: { itemsToShow: 1 },
+    500: { itemsToShow: 5 },
+  },
+};
+const tourCarouselConfig = {
+  itemsToShow: 4,
+  autoplay: 4000,
+  wrapAround: true,
+  pauseAutoplayOnHover: true,
+  snapAlign: 'start',
+};
 </script>
